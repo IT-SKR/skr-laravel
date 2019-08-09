33b2ef645bd4c49149a913fb35b586d90ceec428
liuwei-pbj@hfax.com <liuwei-pbj@hfax.com>
2019-08-09 17:44:18 +0800

    简化调用

--- src/SkrException.php
+++ src/SkrException.php
@@ -9,16 +9,13 @@ class SkrException extends \Exception
     protected $message;
     protected $code;
 
-    public function __construct(string $message,string $code='')
+    public function __construct(string $message,string $code=null)
     {
         $this->message = $message;
         $this->code = $code;
     }
 
     public function render(){
-        if (empty($this->code)){
-            return Skr::response($this->message);
-        }
         return Skr::response($this->message,null,$this->code);
     }
 
